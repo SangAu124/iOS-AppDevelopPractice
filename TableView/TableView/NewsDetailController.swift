06a1a129e7789462e604b6c2a72569c17cb67952
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-08-18 20:56:10 +0900

    NewsDetail
    NewsDetail

---
+++ TableView/TableView/NewsDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsDetailController.swift
+//  TableView
+//
+//  Created by DGSW on 2021/08/18.
+//
+
+import UIKit
+
+class NewsDetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
