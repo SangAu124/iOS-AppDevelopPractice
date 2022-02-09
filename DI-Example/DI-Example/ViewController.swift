1944da4c9ff27a3a34b4e03d6bfbe21aa0fa6a1d
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-02-09 10:22:10 +0900

    🎉 :: pracSwinjectStart

---
+++ DI-Example/DI-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI-Example
+//
+//  Created by 김상금 on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
